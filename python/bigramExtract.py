ca772ae24494dac62756505073b17f93870e3e6f
Sushil <sushilraje8@gmail.com>
2014-04-23 20:38:33 -0400

    Bi grams features

--- python/bigramExtract.py
+++ python/bigramExtract.py
@@ -1,37 +1,46 @@
+
 import string
+from nltk.corpus import stopwords
+
 from nltk.tokenize import word_tokenize
 def get_file_lines(file1, mode='r'):
     file_handler = open(file1, mode)
     file_lines = file_handler.readlines()
     file_handler.close()
     return file_lines
+
+stopword_list = stopwords.words('english')
+
 def find_bigrams(input_list):
-    return zip(input_list, input_list[1:])
-    #return [input_list[i] + " " + input_list[i+1] for i in range(0,(len(input_list)-1))]
+    return [(input_list[i]) + " " + input_list[i+1] + "\n"  for i in range(0,(len(input_list)-1)) if input_list[i] not in stopword_list and input_list[i+1] not in stopword_list]
+
+
 def bi_grams(datafile,idex):
+    curr_sent = 0
+    prev_sent = -1
+    words = []
     try:
-        # read the contents of the whole file into ''filecontents''
-        filecontents=get_file_lines(datafile)
-        file2=open("bigramop.tsv",'w')
-        #file2=open("./feature/Ngrams/bigram_"+ str(idex) + ".tsv", 'w')
-        for filecontent in filecontents:
-            file2.writelines(str(filecontent +"\n")) 
-            # strip all punctuation at the beginning and end of words, and 
-            # convert all words to lowercase
-            words= [ word.strip(string.punctuation).lower() for word in set(word_tokenize(filecontent)) ]
-            file2.writelines(word for word in words)
-            file2.write("\n")                    
-            bigram_dict=find_bigrams(words)
-            for bigram_item in bigram_dict :
-                file2.write(str(bigram_item) + "\n")
+        file_data = get_file_lines(datafile)
+        file2=open("dictionary/tempDictionary.txt",'a')
+        for line in file_data:
+            curr_sent = get_sentence_num(line)
+            if prev_sent != curr_sent:
+                words = [word.strip(string.punctuation).lower().strip() for word in list((word_tokenize(get_sentence(line)))) if str(word.strip(string.punctuation).lower().strip()) != ""]
+                bigram_dict = find_bigrams(words)
+                file2.writelines(bigram_dict)
+            prev_sent = curr_sent
     except IOError:
                 print "I could not find the file, Please try again."
-                exit()      
+                exit()
+
+def get_sentence(line):
+    columns = line.split("\t");
+    return str(columns[2].strip("'"))
+
+def get_sentence_num(lines):
+    columns = lines.split("\t");
+    return int(columns[1].strip("'"))
+
 def main():
-    bi_grams("testtrain.tsv",1)
-    #for i in range(0, 5):
-        #get_word_list(get_file_lines("./data/train_" + str(i) + ".tsv"), i)
-        #bi_grams(("./data/train_" + str(i) + ".tsv"), i)  
-    #train(get_file_lines('tempDictionary.txt'))
-    #classify(get_file_lines('test.tsv'))
+    bi_grams("data/trainML.tsv",1)
 main()
