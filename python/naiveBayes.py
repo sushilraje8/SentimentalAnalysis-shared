b40b5a9d7715dde826c6e20b108306790656b197
Sushil <sushilraje8@gmail.com>
2014-04-27 18:53:13 -0400

    new naive bayes implementation

--- python/naiveBayes.py
+++ python/naiveBayes.py
@@ -30,7 +30,10 @@ def prepare_data():
     for i in range(0, 5):
         print " Fetching and Normalizing Data for class ",i
         lines[i] = get_file_lines("data/train_"+str(i)+"_n.tsv")
-        doc[i] = normalize_data(lines[i])
+        doc[i] = read_normalized_data(i)
+        if doc[i] is None:
+            doc[i] = normalize_data(lines[i])
+            dump_normalized_data(i, doc[i])
         vocab[i] = len(set(doc[i]))
     print "Data ready for training!"
 
@@ -38,20 +41,47 @@ def prepare_data():
 def normalize_data(lines):
     norm_words = []
     punctuation = ['!', '.', ';', ':', '\'', '"','`','?']
-    exceptions = ['\n', '\'s', '\'t', 'n\'t','not', " "]
+    exceptions = ['\n', '\'s', '\'t', " "]
     stemmer = PorterStemmer()
     stop = stopwords.words('english')
+    mega_stop_list = list(itertools.chain(punctuation, exceptions))
     print "    Now Normalizing......."
     for sentence in lines:
-        words = [stemmer.stem(word) for word in word_tokenize(sentence.rstrip("\\n"))]
-        norm_words.extend([word.lower() for word in words if not re.match("[0-9]+", word) if word.lower() not in list(itertools.chain(punctuation, exceptions, stop))])
+        words = [stemmer.stem(word.lower()) for word in word_tokenize(sentence.rstrip("\\n")) if word not in [stop, "not"]]
+        norm_words.extend([word for word in negate_Ngram(words) if not re.match("[0-9]+", word) if word.lower() not in mega_stop_list])
     return norm_words
+
+def negate_Ngram(words):
+    negation = False
+    #delims = "?.,!:;"
+    result = []
+    prev = None
+    pprev = None
+    for word in words:
+        #stripped = word.strip(delims).lower()
+        negated = "not_" + word if negation else word
+        result.append(negated)
+        if prev:
+            bigram = prev + " " + negated
+            result.append(bigram)
+            if pprev:
+                trigram = pprev + " " + bigram
+                #result.append(trigram)
+
+            pprev = prev
+        prev = negated
+        if any(neg in word for neg in ["not", "n't", "no"]):
+            negation = not negation
+        if any(c in word for c in "?.,!:;"):
+            negation = False
+    return result
+
 
 def calculate_class_priori():
     doc_sizes = [len(lines[i]) for i in range(0, 5)]
     return [(1.0*doc_sizes[i]/sum(doc_sizes)) for i in range(0, 5)]
 
-def train():
+def train(threshold):
     global class_priori, class_cond, class_total
     print "Training Data......."
     class_priori = calculate_class_priori()
@@ -64,19 +94,17 @@ def train():
         if class_cond[i] is None:
             class_cond[i] = MyDict()
             for word in doc[i]:
-                if doc[i].count(word) > 2:
-                    class_cond[i][word] = doc[i].count(word) #word frequencies
-
+                if not class_cond[i].has_key(word) and doc[i].count(word) > threshold:
+                        class_cond[i][word] = doc[i].count(word) #word frequencies
                 j += 1
                 if j % 5000 == 0:
                     print j, "Features trained on!"
-                    if j == 50000:
-                        break
-
-
-        dump_features(i, class_cond[i])
-        class_cond[i] = []
+                if j >= 200000:
+                    break
+            dump_features(i, class_cond[i])
         class_total[i] = sum(class_cond[i].values())
+        class_cond[i] = []
+
     print class_priori, class_total, class_cond
 
 def classify(lines):
@@ -84,11 +112,13 @@ def classify(lines):
     matches = 0
     counter = 0
     [sentences, scores] = get_sentence_canonical(lines)
-    for sentence, score in [sentences, scores]:
+    for sentence, score in zip(sentences, scores):
         for i in range(0, 5):
             class_cond[i] = read_features(i)
-            posteriori[i] = sum(log10( (class_cond[i][word] + 1.0)/(class_total[i] + vocab[i]) for word in sentence)  + log10(class_priori[i]) )
-        if score - posteriori.index(max(posteriori)) == 0:
+            log_prob = [log10((class_cond[i][word] + 1.0)/(class_total[i] + vocab[i])) for word in sentence]
+            log_prob.append(log10(float(class_priori[i])))
+            posteriori[i] = sum(log_prob)
+        if int(score) - posteriori.index(max(posteriori)) == 0:
             print sentence, score
             matches += 1
         counter += 1
@@ -96,20 +126,34 @@ def classify(lines):
     print sentences, scores
 
     #sum(class_cond[i].values())
+def dump_normalized_data(classi, doc):
+    nfile = open("data/NaiveBayes_NormData_"+str(classi)+".txt", "w")
+    nfile.write("~!~".join(doc))
+    nfile.close()
+
+def read_normalized_data(classi):
+    if not os.path.exists("data/NaiveBayes_NormData_"+str(classi)+".txt"):
+        return None
+    nfile = open("data/NaiveBayes_NormData_"+str(classi)+".txt", "r")
+    doc = nfile.read().split("~!~")
+    nfile.close()
+    return doc
+
 
 def dump_features(classi, class_condi):
-    nfile = open("data/NaiveBayes_Features_"+str(classi)+".txt", "w")
+    nfile = open("feature/NaiveBayes_Features_"+str(classi)+".txt", "w")
     for word, prob in class_condi.iteritems():
-        nfile.write(word+","+str(prob)+"\n")
+        nfile.write(word+"~!~"+str(prob)+"\n")
     nfile.close()
 
 def read_features(classi):
     temp = MyDict()
-    if not os.path.exists("data/NaiveBayes_Features_"+str(classi)+".txt"):
+    if not os.path.exists("feature/NaiveBayes_Features_"+str(classi)+".txt"):
         return None
-    for line in get_file_lines("data/NaiveBayes_Features_"+str(classi)+".txt"):
-        ld = line.split(",")
-        temp[ld[0]] = int(ld[1].rstrip("\\n"))
+    for line in get_file_lines("feature/NaiveBayes_Features_"+str(classi)+".txt"):
+        ld = line.split("~!~")
+        if ld[0].strip() != "":
+            temp[ld[0].strip()] = float(ld[1].strip().rstrip("\\n"))
     if temp.__sizeof__() == 0:
         return None
     return temp
@@ -129,6 +173,7 @@ def get_sentence_canonical(lines):
 def get_sentence_details(lines, i):
     return lines.split("\t")[i];
 
+
 prepare_data()
-train()
+train(5) #passing threshold a pre-pruning
 classify(get_file_lines("data/test.tsv"))
